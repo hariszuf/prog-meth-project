d5d4f1f2590b199b77139e2d24b23502fac9a467
Ignatius-Pang <ignatius.gac@gmail.com>
2025-10-30 00:34:07 +0800

    Loops Expansion
    Expanded if and else loops  to full statements and edited the GUI words and wrapping to be fully able to be seen

--- tictactoeGUI/main.c
+++ tictactoeGUI/main.c
@@ -5,7 +5,6 @@
 #include "game.h"
 #include "stats.h"
 
-
 static void printBoard(const char b[9]){
     printf("\n %c | %c | %c\n", b[0], b[1], b[2]);
     printf("---+---+---\n");
@@ -22,11 +21,17 @@ int main(void){
 
     printf("Tic Tac Toe Game!\n");
     printf("1. Two Player\n2. Play against AI\nEnter choice: ");
-    if (scanf("%d",&mode)!=1) return 0;
+    if (scanf("%d",&mode)!=1) {
+        return 0;
+    }
     if (mode==2){
         printf("Select AI difficulty (1=Easy 2=Med 3=Hard): ");
-        if (scanf("%d",&level)!=1 || level<1 || level>3) level=2;
-    } else mode=1;
+        if (scanf("%d",&level)!=1 || level<1 || level>3) {
+            level=2;
+        }
+    } else {
+        mode=1;
+    }
 
     while(1){
         printBoard(g.b);
@@ -35,16 +40,25 @@ int main(void){
         } else {
             int pos;
             printf("Player %c, enter position (1-9): ", g.turn);
-            if (scanf("%d",&pos)!=1) return 0;
-            if (!game_make_move(&g, pos-1)){ puts("Invalid move."); continue; }
+            if (scanf("%d",&pos)!=1) {
+                return 0;
+            }
+            if (!game_make_move(&g, pos-1)){
+                puts("Invalid move.");
+                continue;
+            }
         }
         game_check_end(&g);
         if (g.winner){
             printBoard(g.b);
             int winnerCode = (g.winner==1)?1:(g.winner==2)?2:0;
-            if (g.winner==1) puts("X wins!");
-            else if (g.winner==2) puts("O wins!");
-            else puts("Draw!");
+            if (g.winner==1) {
+                puts("X wins!");
+            } else if (g.winner==2) {
+                puts("O wins!");
+            } else {
+                puts("Draw!");
+            }
             StatsMode smode = (mode==1)?STATS_PVP:STATS_PVAI;
             stats_record_result_mode(smode, winnerCode);
             int games,xw,ow,dr; stats_get_counts_mode(smode,&games,&xw,&ow,&dr);
